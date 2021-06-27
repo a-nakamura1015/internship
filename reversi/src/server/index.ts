764ec523b3f3c644448a604405b14f0e6b1e9f34
仲村新太 <nakamurashinta@MacBook-Pro.local>
2021-06-27 21:43:00 +0900

    リバーシの初期表示画面を作成した

--- reversi/src/server/index.ts
+++ reversi/src/server/index.ts
@@ -1,3 +1,7 @@
-function test(): void {
-  console.log('hoge');
+/**
+ * ユーザーがアプリケーションにアクセスした際に実行される関数。
+ * return {HtmlOutput} サニタライズされたHTML
+ */
+function doGet(): GoogleAppsScript.HTML.HtmlOutput {
+  return HtmlService.createTemplateFromFile('client/index').evaluate();
 }
